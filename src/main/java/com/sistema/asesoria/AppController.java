8db5814393b7c41bace76130ed900e54cef5d63a
RC1paula <mprojas548@misena.edu.co>
2022-11-08 19:28:33 -0500

    commit

--- src/main/java/com/sistema/asesoria/AppController.java
+++ src/main/java/com/sistema/asesoria/AppController.java
@@ -2,6 +2,7 @@ package com.sistema.asesoria;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -11,4 +12,8 @@ public class AppController {
 		return "index";
 	}
 	
+	@PostMapping("/","","formularioprincipal")
+	public String principal(){
+		return "formularioprincipal";
+	}
 }
